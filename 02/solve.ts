27b86b9426d7c1ce2655804150b3690e63cc86d5
Rafał Zubala <rzubala@operatus.org>
2019-05-27 09:30:43 +0200

    02 - part1

--- 02/solve.ts
+++ 02/solve.ts
@@ -2,23 +2,47 @@ import * as file from 'fs';
 
 class Checksum {
 
+    data: Array<Array<number>> = new Array();
+
     constructor() {
         this.readData()
         .then((data) => {
-            console.log(data);
+            this.parseData(data);
+            this.log();
+            this.calcSum();
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log('error:', err));
     }
 
     readData = ():Promise<any> => {
         return new Promise((resolve, reject) => {
-            file.readFile('input.data', 'utf8', (data, err) => {
+            file.readFile('input.data', 'utf8', (err, data) => {
                 if (err) {
                     reject(err);
                 }
                 resolve(data);
             });
         });
+    }
+
+    parseData = (fdata: string) => {
+        fdata.split('\n').forEach(line => {
+            this.data.push(line.split('\t').map(c => +c));
+        });
+    }
+
+    log = () => {
+        this.data.forEach(line => {
+            console.log(line.join(' '));
+        })
+    }
+
+    calcSum = () => {
+        let sum: number = 0;
+        this.data.forEach(line => {
+            sum += Math.max(...line) - Math.min(...line);
+        });
+        console.log(sum);
     }
 }
 
