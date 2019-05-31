53ceb3c283fa66eee190dada8a567df64eacfcbb
Rafał Zubala <rzubala@operatus.org>
2019-05-31 09:08:34 +0200

    05 - part 1

--- 05/solve.ts
+++ 05/solve.ts
@@ -5,15 +5,30 @@ class CPUJumps extends FileReader {
 
     constructor() {
         super();
-        this.readData('test.data')
+        this.readData('input.data')
         .then(fdata => {
-            this.data = fdata.split('\n');
+            this.data = fdata.split('\n').map(e => +e);
             this.log();
+            this.calc();
         });
     }
 
     log = () => {
         console.log(this.data.join(','));
+    }
+
+    calc = () => {
+        const len = this.data.length;
+        let p: number = 0;
+        let cnt: number = 0;
+        while(p>=0 && p<len) {
+            const cur:number = this.data[p];
+            this.data[p] = cur + 1;
+            p = p + cur; 
+            //console.log(cnt, p)           
+            cnt += 1;
+        }
+        console.log(cnt);    
     }
 }
 new CPUJumps();
