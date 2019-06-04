859b3a0d1600459b3c370a7fc7d3ad309a6f2f4f
Rafał Zubala <rzubala@operatus.org>
2019-06-04 16:03:02 +0200

    08 - part 2

--- 08/solve.ts
+++ 08/solve.ts
@@ -9,24 +9,53 @@ interface Instruction {
     conditionValue: number;
 }
 
+const Conditions = {
+    '==': (value: number, condition: number):boolean => value === condition,  
+    '!=': (value: number, condition: number):boolean => value !== condition,
+    '>': (value: number, condition: number):boolean => value > condition,
+    '<': (value: number, condition: number):boolean => value < condition,
+    '<=': (value: number, condition: number):boolean => value <= condition,
+    '>=': (value: number, condition: number):boolean => value >= condition,
+}
+
+const Operations = {
+    'inc': (value: number, offset: number): number => value + offset,
+    'dec': (value: number, offset: number): number => value - offset,
+}
+
 class CPU extends FileReader {
 
     private instructions: Array<Instruction> = [];
 
+    private registers: Map<string, number> = new Map();
+    
+    private max: number = 0;
+
     constructor() {
         super();
-        this.readData('test.data')
+        this.readData('input.data')
         .then(fdata => {
-            this.parse(fdata)
-            console.log(this.instructions);
+            this.parse(fdata);   
+            this.process();
+            console.log('final', this.getMax(), this.max);
         })
         .catch(e => console.log('error: ', e));
     }
 
+    getMax = (): number => {
+        return Math.max(...Array.from(this.registers.values()))
+    }
+
+    handleMax = () => {
+        const currentMax: number = this.getMax();
+        if (currentMax > this.max) {
+            this.max = currentMax;
+        }
+    }
+
     parse = (fdata: string) => {
-        this.instructions = fdata.split('\n').map(line => {
-            console.log(line);            
-            const regex: RegExp = /(\w+)\s(inc|dec)\s(-?\d+)\sif\s(\w+)\s((=|<|>|!)+)\s(\d+)/;;
+        this.instructions = fdata.split('\n').map(line => {     
+            const regex: RegExp = /(\w+)\s(inc|dec)\s(-?\d+)\sif\s(\w+)\s((=|<|>|!)+)\s(-?\d+)/;;
             const match: RegExpExecArray = regex.exec(line);
             if (match != null) {
                 const reg: string = match[1];
@@ -34,10 +63,33 @@ class CPU extends FileReader {
                 const offset: number = +match[3];
                 const ref: string = match[4];
                 const cond: string = match[5];
-                const condValue: number = +match[7];
-                return {register: reg, operation: op, offset: offset, reference: ref, condition: cond, conditionValue: condValue};
+                const condValue: number = +match[7];                
+                const instruction: Instruction = {register: reg, operation: op, offset: offset, reference: ref, condition: cond, conditionValue: condValue};
+                return instruction;
             }    
         });
+    }
+
+    process = () => {
+        this.instructions.forEach(i => {
+            let value: number = this.registers.get(i.register);
+            if (!value) {
+                value = 0;                
+            }
+            let referenceValue: number = this.registers.get(i.reference);
+            if (!referenceValue) {
+                referenceValue = 0;
+            }
+            if (this.checkCondition(referenceValue, i.condition, i.conditionValue)) {
+                value = Operations[i.operation](value, i.offset);
+            }
+            this.registers.set(i.register, value);
+            this.handleMax();
+        })
+    }
+
+    checkCondition = (value: number, operation: string, condition: number): boolean => {
+        return Conditions[operation](value, condition);
     }
 }
 new CPU();
