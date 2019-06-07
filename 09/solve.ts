d5174c823209c12aad87f1b8752dac67fe1366e0
Rafał Zubala <rzubala@operatus.org>
2019-06-07 18:42:30 +0200

    09 - part 1

--- 09/solve.ts
+++ 09/solve.ts
@@ -1,14 +1,48 @@
 import { FileReader } from '../common';
 
 class Garbage extends FileReader {
+    private data: Array<string>;
+    private score: number = 0;
+
     constructor() {
         super();
-        this.readData('test.data')
+        this.readData('input.data')
         .then(fdata => {
-            console.log(fdata);
+            this.data = fdata.split('');
+            
+            this.calculateScore();
+            console.log('score: ', this.score);
         })
         .catch(e => console.log('error: ', e))
         ;
+    }
+
+    calculateScore = () => {
+        let level: number = 0;
+        let skipNext: boolean = false;
+        let garbage: boolean = false;
+        this.data.forEach(el => {
+            if (skipNext) {
+                skipNext = false;
+                return;
+            }
+            if (el === '!') {
+                skipNext = true;
+            } else if (!garbage && el === '{') {
+                level += 1;
+            } else if (!garbage && el === '}') {
+                this.updateScore(level);
+                level -= 1;         
+            } else if (el === '<') {       
+                garbage = true;
+            } else if (el === '>') {       
+                garbage = false;
+            }
+        });
+    }
+
+    updateScore = (value: number) => {
+        this.score += value;
     }
 }
 new Garbage();
--- common.ts
+++ common.ts
@@ -12,3 +12,13 @@ export class FileReader {
         });
     }
 }
+
+export class Stack<T> {
+    _store: T[] = [];
+    push(val: T) {
+      this._store.push(val);
+    }
+    pop(): T | undefined {
+      return this._store.pop();
+    }
+  }
