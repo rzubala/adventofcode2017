352fb6bb6dfb2ae51fd438240344218cf02562c4
Rafał Zubala <rzubala@operatus.org>
2019-06-21 14:38:34 +0200

    16 - parse input

--- 16/solve.ts
+++ 16/solve.ts
@@ -1,11 +1,30 @@
 import { FileReader } from '../common'
 
+interface Operation {
+    op: string;
+    data1?: string;
+    data2?: string;
+}
+
 class Solve extends FileReader {
+
+    private operations: Operation[];
+
     constructor() {
         super();
-        this.readData('test.data')
+        this.readData('input.data')
         .then(fdata => {
             console.log(fdata);
+            this.operations = fdata.split(',').map(e => {
+                const tmp: string[] = e.split('');
+                if (tmp[0] === 's') {
+                    return {op: tmp[0], data1: tmp.slice(1)};
+                } else  {
+                    const data: string[] = e.substring(1).split('/');
+                    return {op: tmp[0], data1: data[0], data2: data[1]};
+                }
+            });
+            this.operations.forEach(o => console.log(o));
         })
         .catch(e => console.log('error: ', e));
     }
