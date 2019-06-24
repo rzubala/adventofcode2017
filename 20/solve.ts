bb87ff0b4e32eb0db5ca1b356b2cf181392e4b4d
Rafał Zubala <rzubala@operatus.org>
2019-06-24 14:55:38 +0200

    20 - parse data

--- 20/solve.ts
+++ 20/solve.ts
@@ -1,13 +1,47 @@
 import { FileReader } from '../common'
 
+interface Data {
+    x: number;
+    y: number;
+    z: number;
+}
+
 class Solve extends FileReader {
+
+    private p: Data[] = [];
+    private v: Data[] = [];
+    private a: Data[] = [];
+
     constructor() {
         super();
         this.readData('test.data')
         .then(fdata => {
-            console.log(fdata);
+            this.parse(fdata);
+            this.process();
         })
         .catch(e => console.log('error: ', e));
+    }
+
+    private process = () => {
+        
+    }
+
+    private parse = (fdata) => {
+        fdata.split('\n').forEach(line => {
+            const tmp: string[] = line.split(', ');
+            this.p.push(this.parseData(tmp[0]));
+            this.v.push(this.parseData(tmp[1]));
+            this.a.push(this.parseData(tmp[2]));
+        });
+    }
+
+    private parseData = (data: string): Data => {
+        data = data.replace('p=<', '');
+        data = data.replace('v=<', '');
+        data = data.replace('a=<', '');
+        data = data.replace('>', '');
+        const tmp: string[] = data.split(',');
+        return {x: +tmp[0],y: +tmp[1], z: +tmp[2]};
     }
 }
 new Solve();
