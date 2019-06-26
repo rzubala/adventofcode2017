7c7cd3b093e842ef3a5da8219d7d5e76419397d1
Rafał Zubala <rzubala@gmail.com>
2019-06-26 08:30:04 +0200

    21 - part1 initial

--- 21/solve.ts
+++ 21/solve.ts
@@ -1,15 +1,110 @@
 import { FileReader } from '../common'
 
 class Solve extends FileReader {
+
+    private patterns: Map<String, String> = new Map();
+
     constructor() {
         super();
         this.readData('test.data')
         .then(fdata => {
-            //console.log(fdata);
+            this.parsePatterns(fdata);
+            this.process();
 
-            this.test();
+            //this.test();
         })
         .catch(e => console.log('error: ', e));
+    }
+
+    private process = () => {
+        const start: string = ".#./..#/###";
+        let matrix: string[][] = this.parse(start);
+        let size: number = matrix.length;
+        
+        if (size % 2 === 0) {
+            matrix = this.handleSubMatrix(2, matrix, size);
+        } else if (size % 3 === 0) {
+            matrix = this.handleSubMatrix(3, matrix, size);
+        }
+    }
+
+    private handleSubMatrix = (subSize: number, matrix: string[][], size: number): string[][] => {
+        const result:string[][] = [];
+        for (let i=0;i<size/subSize;i++) {
+            for (let j=0;j<size/subSize;j++) {
+                const subMatrix: string[][] = this.getSubMatrix(matrix, size, subSize, i, j);
+                this.convert(result, subMatrix, subSize, i, j)
+            }
+        }
+        return result;
+    }
+
+    private convert = (matrix: string[][], source: string[][], subSize: number, gi: number, gj: number) => {
+        const converted: string = this.convertMatrix(source, subSize);
+        if (converted === undefined) {
+            console.log('ERROR')
+        } else {
+            this.log(this.parse(converted));
+        }
+    }
+
+    private convertMatrix = (matrix: string[][], subSize: number): string => {
+        const size:number = matrix.length;
+
+        console.log('source')
+        this.log(matrix);
+
+        for (let pattern in this.patterns) {
+            const mPattern: string[][] = this.parse(pattern);
+
+            console.log('pattern')
+            this.log(mPattern);
+
+            if (mPattern.length !== size) {
+                continue;
+            }
+
+            if (this.compare(matrix, mPattern)) {
+                return this.patterns[pattern];
+            }
+            if (this.compare(matrix, this.flip(mPattern, true))) {
+                return this.patterns[pattern];
+            }
+            if (this.compare(matrix, this.flip(mPattern, false))) {
+                return this.patterns[pattern];
+            }         
+            let rotated:string[][] = this.rotate(mPattern);
+            if (this.compare(matrix, rotated)) {
+                return this.patterns[pattern];
+            }
+            rotated = this.rotate(rotated);
+            if (this.compare(matrix, rotated)) {
+                return this.patterns[pattern];
+            }                
+            rotated = this.rotate(rotated);
+            if (this.compare(matrix, rotated)) {
+                return this.patterns[pattern];
+            }
+        }
+        return undefined;
+    }
+
+    private getSubMatrix = (matrix:string[][], size: number, newSize: number, gi:number, gj: number):string[][] => {
+        const result:string[][] = [];
+        for (let i=0;i<newSize;i++) {
+            result.push([])
+            for (let j=0;j<newSize;j++) {
+                result[i][j] = matrix[gi*newSize + i][gj*newSize + j];
+            }
+        }
+        return result;
+    }
+
+    private parsePatterns = (fdata) => {
+        fdata.split('\n').forEach(l => {
+            const tmp: string[] = l.split(' => ');
+            this.patterns[tmp[0]] = tmp[1];
+        })
     }
 
     private flip = (matrix: string[][], horizontal: boolean): string[][] => {
@@ -68,6 +163,14 @@ class Solve extends FileReader {
         return matrix;
     }
 
+    private flat = (matrix: string[][]): string => {
+        let result: string = "";
+        matrix.forEach(r => {
+            result += r.join('') + '/';            
+        })
+        return result.substring(0, result.length - 1);
+    }
+
     private test = () => {
         const matrix: string[][] = [];
         const size: number = 3;
@@ -98,6 +201,25 @@ class Solve extends FileReader {
         console.log('test parse');
         const m: string[][] = this.parse('123/456/789');
         this.log(m);
+
+        console.log('test flat');
+        console.log(this.flat(m));
+
+        console.log('test submatrix');
+        const bigm: string[][] = this.parse('123abc/456def/789ghi/123jkl/456mno/789prq');
+        this.log(bigm);
+
+        console.log('sub', '0','0')
+        this.log(this.getSubMatrix(bigm, 6, 3, 0, 0));
+
+        console.log('sub', '0','1')
+        this.log(this.getSubMatrix(bigm, 6, 3, 0, 1));
+
+        console.log('sub', '1','0')
+        this.log(this.getSubMatrix(bigm, 6, 3, 1, 0));
+
+        console.log('sub', '1','1')
+        this.log(this.getSubMatrix(bigm, 6, 3, 1, 1));
     }
 
     private log = (matrix: string[][]) => {
