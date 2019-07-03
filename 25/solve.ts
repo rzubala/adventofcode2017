314fa171900e6ee3bbd2282db4e11e90c1f8866e
Rafał Zubala <rzubala@gmail.com>
2019-07-03 21:57:13 +0200

    25 - test

--- 25/solve.ts
+++ 25/solve.ts
@@ -1,13 +1,74 @@
 import { FileReader } from '../common'
 
 class Solve extends FileReader {
-    constructor() {
-        super();
-        this.readData('test.data')
-        .then(fdata => {
-            console.log(fdata);
-        })
-        .catch(e => console.log('error: ', e));
+
+    private tape: Map<number, boolean> = new Map();
+
+    private step: number = 0;
+
+    private limit: number = 6;
+
+    private states = {
+        'A': (cursor: number) => { this.stateA(cursor) },
+        'B': (cursor: number) => { this.stateB(cursor) }
+    }
+
+    public start = () => {
+        this.nextState('A', 0);
+    }
+
+    private stateA = (cursor: number) => {
+        const value: boolean = this.get(cursor);
+        console.log('A:', value, cursor)
+        if (!value) {
+            this.tape.set(cursor, true);
+            this.nextState('B', cursor + 1);
+        } else {
+            this.tape.set(cursor, false);
+            this.nextState('B', cursor - 1);
+        }
+    }
+
+    private stateB = (cursor: number) => {
+        const value: boolean = this.get(cursor);
+        console.log('B:', value, cursor)
+        if (!value) {
+            this.tape.set(cursor, true);
+            this.nextState('A', cursor - 1);
+        } else {
+            this.tape.set(cursor, true);
+            this.nextState('A', cursor + 1);
+        }
+    }
+
+    private nextState = (state: string, cursor: number) => {
+        if (this.step++ === this.limit) {
+            console.log('END');
+            return;
+        }
+        console.log('next:', state, cursor);
+        this.states[state](cursor);
+    }
+
+    private get = (cursor: number): boolean => {
+        const value: boolean = this.tape.get(cursor);
+        if (value === undefined) {
+            return false;
+        }
+        return value;
+    }
+
+    public sum = () => {
+        let result:number = 0;
+        this.tape.forEach((value: boolean, key: number) => {
+            console.log(key, value);
+            if (value) {
+                result++;
+            }
+        });
+        console.log('sum: ', result)
     }
 }
-new Solve();
+const solve: Solve = new Solve();
+solve.start();
+solve.sum();
